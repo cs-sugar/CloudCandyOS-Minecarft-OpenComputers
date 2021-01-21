d7f621ef3377d2862675395d6ec6034b0bb957e1
Rs-sugar <61970096+cs-sugar@users.noreply.github.com>
2021-01-21 00:16:57 -0600

    Update Main.lua

--- Applications/App Market.app/Main.lua
+++ Applications/App Market.app/Main.lua
@@ -91,7 +91,7 @@ local leftList = window:addChild(GUI.list(1, 4, leftListPanel.width, 1, 3, 0, 0x
 
 local contentContainer = window:addChild(GUI.container(1, 1, 1, 1))
 
-local sponsoredLabel = window:addChild(GUI.text(2, 1, 0x3C3C3C, "Sponsored by McModder"))
+local sponsoredLabel = window:addChild(GUI.text(2, 1, 0x3C3C3C, "Sponsored by CloudCandy"))
 
 local progressIndicator = window:addChild(GUI.progressIndicator(math.floor(leftListPanel.width / 2 - 1), 1, 0x3C3C3C, 0x00B640, 0x99FF80))
 
